f0fe1ef88d9c473f5b454429cc665800c93d28c7
jyoti <jyotikumari123>
2022-12-07 02:33:34 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import "../styles/App.css";
 import Component from "./Component";
 import ErrorBoundary from "./ErrorBoundary";
 const App = () => {
-
-
+  errorGenerator()
+  changeFunc()
+    
   return (
 
     <div id="main">
--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,13 +1,22 @@
 import React from "react";
 
 class ErrorBoundary extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={ hasError : false };
+  }
+
+  static getDerivedStateFromError(error){
+    return { hasError : true };
+  }
  
 
   render() {
     if (this.state.hasError) {
       return (
         <p id="error">
-          
+          There is an error in your react app. Please Check again. This message has been genrated from error boundary.
         </p>
       );
     }
